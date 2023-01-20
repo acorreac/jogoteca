a6e4695abae266db10eb61e1c773e07267fccede
acorreac <aline.cristianecorrea@hotmail.com>
2023-01-19 21:05:26 -0300

    Melhorando destino de upload

--- views.py
+++ views.py
@@ -32,7 +32,8 @@ def criar():
     db.session.commit()
 
     arquivo = request.files['arquivo']
-    arquivo.save(f'uploads/{arquivo.filename}')
+    upload_path = app.config['UPLOAD_PATH']
+    arquivo.save(f'{upload_path}/capa-{novo_jogo.nome}.jpg')
 
     return redirect(url_for('index'))
 
